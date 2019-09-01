6693cfccbe4e86b9b063bad73e5a2771e8d2b531
malufreitas <malufrt@Hotmail.com>
2019-09-01 17:54:41 -0300

    analisando tweets de um usuario
    tentando pegar tweets apartir de uma palavra

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -4,9 +4,16 @@ from tweepy.streaming import StreamListener #permite 'ouvir' o twitter
 from tweepy import OAuthHandler #permite autenticação do twitter app
 from tweepy import Stream 
 
-import twitter_credentials
+from textblob import TextBlob  # Para analise de sentimentos
+
+import twitter_credentials  # Para credenciais do twitter
 import numpy as np   # Para analisar os dados do twitter
 import pandas as pd  # Para analisar os dados do twitter
+import matplotlib.pyplot as plt  # Para gráficos
+import re  # Para expressão regular
+
+import json
+
 
 # # # Twitter Client # # #
 class TwitterClient():
@@ -17,6 +24,7 @@ class TwitterClient():
         self.twitter_client = API(self.auth)
         
         self.twitter_user = twitter_user
+
     
     def get_twitter_client_api(self):
         return self.twitter_client
@@ -109,56 +117,110 @@ class TweetAnalyzer():
     Functionality for analyzing and categorizing content from tweets.
     """
 
+    # Limpa o tweet com uma expressão regular
+    def clean_tweet(self, tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+    def analyze_sentiment(self, tweet):
+        analysis = TextBlob(self.clean_tweet(tweet))
+        
+        if analysis.sentiment.polarity > 0:  # Positivo
+            return 1
+        elif analysis.sentiment.polarity == 0:  # Neutro
+            return 0
+        else:  # Negativo
+            return -1
+
+
     # Converte os tweets para data frame
     def tweets_to_data_frame(self, tweets):
         # Pega o text do tweet, e faz isso com todos os tweets (loop)
-        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['Tweets'])
+        df = pd.DataFrame(data=[tweet.text for tweet in tweets], columns=['tweets'])
 
         # Criando colunas no data frame
         # Fazendo um loop para cada tweet da lista de tweets
-        df['id'] = np.array([tweet.id for tweet in tweets])
-        df['len'] = np.array([len(tweet.text) for tweet in tweets])
-        df['date'] = np.array([tweet.created_at for tweet in tweets])
-        df['source'] = np.array([tweet.source for tweet in tweets])
-        df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
-        df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
+        #df['id'] = np.array([tweet.id for tweet in tweets])
+        #df['len'] = np.array([len(tweet.text) for tweet in tweets])
+        #df['date'] = np.array([tweet.created_at for tweet in tweets])
+        #df['source'] = np.array([tweet.source for tweet in tweets])
+        #df['likes'] = np.array([tweet.favorite_count for tweet in tweets])
+        #df['retweets'] = np.array([tweet.retweet_count for tweet in tweets])
 
         return df
 
 
 if __name__ == "__main__":
-    """
     # Palavras para filtrar no twitter
-    hash_tag_list = ['donald trump', 'hillary clinton']
+    hash_tag_list = ['xiaomi mi9']
     fetched_tweets_filename = "tweets.txt"
-    #fetched_tweets_filename = "teste.json"
-
-    # Cria o objeto twitter_client
-    twitter_client = TwitterClient('pycon')
 
     # Printar a função de pegar tweets
-    #### Ta retornando vazio
-    print(twitter_client.get_user_timeline_tweets(1))
+    #print(twitter_client.get_user_timeline_tweets(1))
 
-    #twitter_streamer = TwitterStreamer()
-    #twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
-    """
-    twitter_client = TwitterClient()
-    tweet_analyzer = TweetAnalyzer()
+    twitter_streamer = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
 
-    api = twitter_client.get_twitter_client_api()
+    #twitter_client = TwitterClient()
+    #tweet_analyzer = TweetAnalyzer()
 
+    #api = twitter_client.get_twitter_client_api()
+    
     # A função "user_timeline()" é uma função do tweepy API, já pronta do twitter client
-    tweets = api.user_timeline(screen_name="realDonaldTrump", count=20)
-    # Alguns testes mostrando o que podemos extrair de cada tweet
-    #print(tweets)
-    #print(tweets[0])
-    #print(dir(tweets[0]))
-    #print(tweets[0].id)
-    #print(tweets[0].retweet_count)
-
+    '''
+    tweets = api.user_timeline(screen_name="realDonaldTrump", count=200)
+    
+    #tweets = twitter_streamer.filter(follow=None, track=hash_tag_list, languages=["pt"])
 
     df = tweet_analyzer.tweets_to_data_frame(tweets)
+    df['sentiment'] = np.array([tweet_analyzer.analyze_sentiment(tweet) for tweet in df['tweets']])
+
     print(df.head(10))
+    #print(df)
+    '''
     
+    '''
+    #Alguns testes mostrando o que podemos extrair de cada tweet
+    print(tweets)
+    print(tweets[0])
+    print(dir(tweets[0]))
+    print(tweets[0].id)
+    print(tweets[0].retweet_count)
+    '''
+
+    '''
+    df = tweet_analyzer.tweets_to_data_frame(tweets)
+    #print(df.head(10))
+    #print(df)
+
+    # Pega o tamanho médio de todos os tweets
+    print(np.mean(df['len']))
+
+    # Pega o número de likes do tweet mais curtido
+    print(np.max(df['likes']))
+
+    # Pega o número de retweets do tweet mais retweetado
+    print(np.max(df['retweets']))
+
+    # Time Series
+    #time_likes = pd.Series(data=df['len'].values, index=df['date'])
+    #time_likes.plot(figsize=(16, 4), color='r')
+    #plt.show()
+    
+    #time_favs = pd.Series(data=df['likes'].values, index=df['date'])
+    #time_favs.plot(figsize=(16, 4), color='r')
+    #plt.show()
+
+    #time_retweets = pd.Series(data=df['retweets'].values, index=df['date'])
+    #time_retweets.plot(figsize=(16, 4), color='r')
+    #plt.show()
+
+    # Layered Time Series:
+    #time_likes = pd.Series(data=df['likes'].values, index=df['date'])
+    #time_likes.plot(figsize=(16, 4), label="likes", legend=True)
+
+    #time_retweets = pd.Series(data=df['retweets'].values, index=df['date'])
+    #time_retweets.plot(figsize=(16, 4), label="retweets", legend=True)
+    #plt.show()
+    '''
+
     
